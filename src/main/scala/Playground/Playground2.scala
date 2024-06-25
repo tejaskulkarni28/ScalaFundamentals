22acd0358ad958965b6239577f991c83c3d9c273
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-06-26 00:46:40 +0530

    Getting familiar with methods in scala

---
+++ src/main/scala/Playground/Playground2.scala
@@ -0,0 +1,10 @@
+package Playground
+
+object Playground2 extends App{
+  val firstName: String = "Tejas"
+  val lastName: String = "Kulkarni"
+  val designation: String = "Scala Developer"
+  val joiningDate: String = "01/01/2001"
+
+  println("Welcome " + firstName + " " + lastName + " as our new joiner, who had joined on " + joiningDate + " as a " + designation + ". ")
+}
