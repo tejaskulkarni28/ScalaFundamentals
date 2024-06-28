0b01f4852e5d36a2582b7ab6c0d4183edf3b1ff6
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-06-28 23:41:29 +0530

    Playground4: Playing around default args

---
+++ src/main/scala/Playground/Playground4.scala
@@ -0,0 +1,10 @@
+package Playground
+
+object Playground4 extends App{
+  // create a function name: DefaultArgs
+  // after that I will test around the code how exactly does the default args works and which doesn't
+  def DefaultArgs(name: String, age: Int, lastname: String):Unit={
+    println(name + " " + age + " " + " " + lastname)
+  }
+  DefaultArgs(age = 22, name = "Tejas", lastname = "Kulkarni")
+}
