7a4e450a2308e6eea4d7fb77f09b91af300294b0
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-07-02 01:18:34 +0530

    Playing around functions and methods

---
+++ src/main/scala/Revision/PlaywithFunctions.scala
@@ -0,0 +1,11 @@
+package Revision
+
+object PlaywithFunctions extends App {
+  // pure function: takes input and returns output, no silly process
+  // below function can be called the anonymous function
+  private val add = (a:Int, b: Int) => a + b
+  println(add(2, 2))
+
+  private val subtract = (a: Int, b: Int) => a - b
+  println(subtract(4, 3))
+}
